eedb523366a25af8c0521f6651486a0f682aa9fb
sskt98 <sskt0965@gmail.com>
2026-04-21 19:48:30 +0300

    fix media

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
     path('api/v1/payment/', include('apps.payment.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

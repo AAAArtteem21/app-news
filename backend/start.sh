cb6b74a064b312963b602d8f0e8afe06de71a7b0
sskt98 <sskt0965@gmail.com>
2026-04-22 11:45:30 +0300

    sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 python manage.py create_subscription_product
 python manage.py fix_stripe_integration
-
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3

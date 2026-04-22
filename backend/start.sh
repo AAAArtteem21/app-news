88e471cabb3f07ef4ea38850df2c6a4d408e7f1c
sskt98 <sskt0965@gmail.com>
2026-04-22 11:56:40 +0300

    ls

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python manage.py migrate --noinput
-python -c "import django.contrib.admin; import os; print(os.path.join(os.path.dirname(django.contrib.admin.__file__), 'static'))"
-python manage.py collectstatic --noinput --clear -v 3 2>&1 | head -100
+python manage.py collectstatic --noinput --clear
+ls -la /app/staticfiles/
 python manage.py create_subscription_product
 python manage.py fix_stripe_integration
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3

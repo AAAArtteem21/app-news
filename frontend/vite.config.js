533af33ee1f333faea669b18d17ea149e7d6d42a
sskt98 <sskt0965@gmail.com>
2026-04-22 10:00:45 +0300

    fix redirect

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
   plugins: [
     vue(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        copyFileSync('public/_redirects', 'dist/_redirects')
+      }
+    }
   ],
   resolve: {
     alias: {
